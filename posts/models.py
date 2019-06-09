0d9de5d1187d4afd811d8ab16a520dd03557ef37
sanjokigc <sanjok.dangol@igctech.com.np>
2019-06-09 17:27:43 +0545

    update posts model

--- posts/models.py
+++ posts/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Posts(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.CharField
-    excerpt = models.TextField
-    content = models.TextField
+    title = models.CharField(max_length = 200)
+    slug = models.SlugField(default='')
+    link = models.URLField(default='')
+    excerpt = models.TextField(blank=False, default= '')
+    content = models.TextField(blank=False, default= '')
+    # image = models.ImageField(default="Null")
     publish_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.title
